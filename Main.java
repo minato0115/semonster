15831d15d3fcbbacade2c66380189275e92681e8
unknown <m1m22a27@st.oit.ac.jp>
2022-06-13 10:10:29 +0900

    .classpath

--- Main.java
+++ Main.java
@@ -2,6 +2,9 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("SEMonster!");
     Monster monster = new Monster();
+    Player player = new Player();
     System.out.println(monster.toString());
+    player.drawMonsters();
+    player.showDeck();
   }
 }
